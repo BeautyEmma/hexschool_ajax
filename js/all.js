3760d4f1912013829b7ac973d2ccc4da4b39d355
ItsEms <emma801006@hotmail.com>
2017-03-02 15:39:22 +0800

    modify js
    async打錯..datatype是dataType zzz

--- js/all.js
+++ js/all.js
@@ -5,9 +5,9 @@ $(function(){
         $('#js-tbody').html(''); 
    $.ajax({
      type  : 'GET',
-     url   : 'data.json',
-     asynce:false,
-     datatype:'json',
+     url   : 'http://opendata.khcc.gov.tw/public/OD_art_program.ashx?SDate=2017/02/18&EDate=2017/08/18',
+     async:false,
+     dataType:'json',
      success: function (data) {
              var thisdata = JSON.parse(data);
              var con = $('#select').val();
